fa1faf69a6c65c5647980828b27d6e5c192499b0
gogowonji <65698313+gogowonji@users.noreply.github.com>
2021-10-08 14:29:08 +0900

    setContentView(R.layout.activity_main)

--- app/src/main/java/com/example/graphicpractice/CustomView.java
+++ app/src/main/java/com/example/graphicpractice/CustomView.java
@@ -9,40 +9,87 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class CustomView extends View {
+    //TODO : Add Constants
+    private Paint mPaints, mFramePaint;
+    private RectF mBigOval;
+    private float mStart, mSweep;
+    private static final float SWEEP_INC = 2;
+    private static final float START_INC = 15;
+
     public CustomView(Context context){
+        //TODO : Create src
         super(context);
-        setBackgroundColor(Color.YELLOW);
+        mPaints = new Paint();
+        mPaints.setAntiAlias(true);
+        mPaints.setStyle(Paint.Style.FILL);
+        mPaints.setColor(0x88FF0000);
+        mFramePaint = new Paint();
+        mFramePaint.setAntiAlias(true);
+        mFramePaint.setStyle(Paint.Style.STROKE);
+        mFramePaint.setStrokeWidth(3);
+        mFramePaint.setColor(0x8800FF00);
+        mBigOval = new RectF(100, 40, 900, 1000);
     }
-    //XML 에서 뷰를 참조하려면 생성자 구현해야 함
+
+    /** XML 에서 뷰를 참조하려면 생성자 구현해야 함 **/
     public CustomView(Context context, AttributeSet attrs){
+        //TODO : Create src
         super(context);
-        setBackgroundColor(Color.YELLOW);
+        mPaints = new Paint();
+        mPaints.setAntiAlias(true);
+        mPaints.setStyle(Paint.Style.FILL);
+        mPaints.setColor(0x88FF0000);
+        mFramePaint = new Paint();
+        mFramePaint.setAntiAlias(true);
+        mFramePaint.setStyle(Paint.Style.STROKE);
+        mFramePaint.setStrokeWidth(3);
+        mFramePaint.setColor(0x8800FF00);
+        mBigOval = new RectF(100, 40, 900, 1000);
+
+
     }
     @Override
     protected void onDraw(Canvas canvas) {
-        Paint paint = new Paint();
-        // initial color
-        paint.setColor(Color.YELLOW);
-        //background color
-        canvas.drawColor(Color.BLUE);
-        // yellow rec
-        canvas.drawRoundRect(new RectF(30,50,330,550), 15, 15, paint);
-        // yellow cir
-        canvas.drawOval(new RectF(450,50,750,550), paint);
-        // red cir
-        paint.setColor(Color.RED);
-        canvas.drawArc(new RectF(30,600,330,1100), 360, 1000,
-                true, paint);
-        //line
-        paint.setColor(Color.YELLOW);
-        float[] pts={ 30, 1250,
-                300, 1350,
-                300, 1350,
-                60, 1450,
-                60, 1450,
-                360, 1500};
-        paint.setStrokeWidth(10);
-        canvas.drawLines(pts, paint);
+        //TODO : Add source New on Draw
+        canvas.drawColor(Color.YELLOW);
+        canvas.drawRect(mBigOval, mFramePaint);
+        canvas.drawArc(mBigOval, mStart, mSweep, false, mPaints);
+        mSweep += SWEEP_INC;
+        if (mSweep > 360) {
+            mSweep -= 360;
+            mStart += START_INC;
+            if (mStart >= 360)
+                mStart -= 360;
+        }
+        //다시 onDraw() 호출 호출안하면 안 그려짐
+        invalidate();
+
+
+//        Paint paint = new Paint();
+//        // initial color
+//        paint.setColor(Color.YELLOW);
+//        //background color
+//        canvas.drawColor(Color.BLUE);
+//        // yellow rec
+//        canvas.drawRoundRect(new RectF(30,50,330,550), 15, 15, paint);
+//        // yellow cir
+//        canvas.drawOval(new RectF(450,50,750,550), paint);
+//        // red cir
+//        paint.setColor(Color.RED);
+//        canvas.drawArc(new RectF(30,600,330,1100), 360, 1000,
+//                true, paint);
+//        //line
+//        paint.setColor(Color.YELLOW);
+//        float[] pts={ 30, 1250,
+//                300, 1350,
+//                300, 1350,
+//                60, 1450,
+//                60, 1450,
+//                360, 1500};
+//        paint.setStrokeWidth(10);
+//        canvas.drawLines(pts, paint);
+
+
     }
 
 }
--- app/src/main/java/com/example/graphicpractice/MainActivity.java
+++ app/src/main/java/com/example/graphicpractice/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /** AttributeSet attrs 파라메타 있는 곳에서 변수들 선언해주어야 xml파일과 연결 가능 **/
         setContentView(R.layout.activity_main);
+
+        //public CustomView(Context context)를 활용했기 때문에 당연히 정상작동
+        //setContentView(new CustomView(this));
     }
 }
